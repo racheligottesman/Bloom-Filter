11b1c83b3cf51794d2ab7c843aad0591b79b3228
racheligottesman <97412421+racheligottesman@users.noreply.github.com>
2022-07-22 09:32:02 +0300

    Update BloomFilterHW.py

--- BloomFilterHW.py
+++ BloomFilterHW.py
@@ -2,14 +2,10 @@ from BitHash import BitHash
 from BitVector import BitVector
 
 class BloomFilter(object):
-    # Return the estimated number of bits needed (N in the slides) in a Bloom 
-    # Filter that will store numKeys (n in the slides) keys, using numHashes 
-    # (d in the slides) hash functions, and that will have a
-    # false positive rate of maxFalsePositive (P in the slides).
-    # See the slides for the math needed to do this.  
-    # You use Equation B to get the desired phi from P and d
-    # You then use Equation D to get the needed N from d, phi, and n
-    # N is the value to return from bitsNeeded
+    # Return the estimated number of bits needed in a Bloom 
+    # Filter that will store numKeys keys, using numHashes 
+    # hash functions, and that will have a
+    # false positive rate of maxFalsePositive.
     def __bitsNeeded(self, numKeys, numHashes, maxFalsePositive):
         
         phi = 1 - (maxFalsePositive**(1/numHashes))
@@ -22,9 +18,8 @@ class BloomFilter(object):
     # rate of maxFalsePositive.
     # All attributes must be private.
     def __init__(self, numKeys, numHashes, maxFalsePositive):
-         # will need to use __bitsNeeded to figure out how big
+        # will need to use __bitsNeeded to figure out how big
         # of a BitVector will be needed
-        # In addition to the BitVector, might you need any other attributes?
         self.__bitVector = BitVector(size = int(self.__bitsNeeded(numKeys, numHashes, maxFalsePositive)))
         self.__numKeys = numKeys
         self.__numHashes = numHashes
@@ -34,7 +29,6 @@ class BloomFilter(object):
     # insert the specified key into the Bloom Filter.
     # Doesn't return anything, since an insert into 
     # a Bloom Filter always succeeds!
-    # See the "Bloom Filter details" slide for how insert works.
     def insert(self, key):
         for i in range(1, self.__numHashes+1):
             hashVal = BitHash(key, i) % len(self.__bitVector)
@@ -44,7 +38,6 @@ class BloomFilter(object):
     
     # Returns True if key MAY have been inserted into the Bloom filter. 
     # Returns False if key definitely hasn't been inserted into the BF.
-    # See the "Bloom Filter details" slide for how find works.
     def find(self, key):
         for i in range(1, self.__numHashes+1):
             hashVal = BitHash(key, i) % len(self.__bitVector)
@@ -55,9 +48,6 @@ class BloomFilter(object):
     # ACTUAL current number of bits actually set in this Bloom Filter. 
     # This is NOT the same thing as trying to use the Bloom Filter and
     # measuring the proportion of false positives that are actually encountered.
-    # In other words, you use equation A to give you P from d and phi. 
-    # What is phi in this case? it is the ACTUAL measured current proportion 
-    # of bits in the bit vector that are still zero. 
     def falsePositiveRate(self):
         totalZero = 0
         for i in self.__bitVector:
@@ -67,13 +57,9 @@ class BloomFilter(object):
         return (1 - phi) ** self.__numHashes
        
     # Returns the current number of bits ACTUALLY set in this Bloom Filter
-    # WHEN TESTING, MAKE SURE THAT YOUR IMPLEMENTATION DOES NOT CAUSE
-    # THIS PARTICULAR METHOD TO RUN SLOWLY.
     def numBitsSet(self):
         return self.__numSet 
         
-
-
        
 
 def __main():
